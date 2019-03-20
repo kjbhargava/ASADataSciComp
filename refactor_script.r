4d46f5e7b7ba2e5d060dc06bea66610a7b2a0b96
Lanceypantsy <34278955+Lanceypantsy@users.noreply.github.com>
2019-03-20 16:53:54 -0600

    Update refactor_script.r

--- refactor_script.r
+++ refactor_script.r
@@ -1,6 +1,8 @@
 # The following is a function to change numerical values to appropriate
 # categorical (named) values.
 #
+# There is also a .ipynb file that you can look at an implementation of these tools
+#
 # The inputs are as follows:
 # orig_df - This is the unaltered dataframe from your .csv import (df object)
 # codebook_df - This is dataframe that represents your 'dictionary' (df object)
@@ -10,7 +12,10 @@
 # Note that old_name and new_name, have to match the column names specified in your 
 # codebook dataframe object
 
-refactor_function <- function(orig_df, 
+
+# This function appends the year column, is it only for the last 
+# dataframe you will append to your df
+rf_func_year <- function(orig_df, 
                                 codebook_df, 
                                 orig_name,
                                 new_name) {
@@ -19,5 +24,18 @@ refactor_function <- function(orig_df,
     df <- select(df, -c(orig_name))
     colnames(df)[colnames(df) == orig_name] <- new_name
     df <- select(df, new_name, year)
+    return(df)
+}
+
+# This function doesn't append the year and it should be used for most
+rf_func <- function(orig_df, 
+                                codebook_df, 
+                                orig_name,
+                                new_name) {
+    
+    df <- left_join(x = codebook_df, y = orig_df, by = orig_name)
+    df <- select(df, -c(orig_name))
+    colnames(df)[colnames(df) == orig_name] <- new_name
+    df <- select(df, new_name)
     return(df)
 }
