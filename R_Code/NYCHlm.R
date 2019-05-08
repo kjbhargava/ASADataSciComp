fa9d790c79c3631d54024c16ccc85a38628adb23
Lanceypantsy <34278955+Lanceypantsy@users.noreply.github.com>
2019-05-08 10:53:29 -0600

    Update NYCHlm.R
    Cleaned up comments a little. Changed path to read files as appropriate so code can be ran by someone who clones the directory.

--- R_Code/NYCHlm.R
+++ R_Code/NYCHlm.R
@@ -1,31 +1,43 @@
 
-##input files
-owned <- read.csv("C:\\Users\\maybe\\Downloads\\ownedNYCHVS_midterm2.csv", header = T)
-rented <- read.csv("C:\\Users\\maybe\\Downloads\\rentedNYCHVS_midterm3.csv", header = T)
+# Read in input files
+owned <- read.csv("../ownedNYCHVS_midterm2.csv", header = T)
+rented <- read.csv("../rentedNYCHVS_midterm3.csv", header = T)
 
-##owned linear models
-#meaningful prediction, but low R value
+# Linear Models for the 'owned' dataset
+# A model with a meaningful prediction, but low R value
 myRegOwnedinc <- lm(hhinc ~ value + num_rooms + num_bedrooms + num_under18, data = owned)
-#meaningless prediction, but bst R value could find
+
+# A meaningless prediction, but bst R value that could be found
 myRegOwnedbed <- lm(num_bedrooms ~  num_rooms + num_stories + num_under18, data = owned)
 
-##rented linear models
-#meaningful prediction, but low R value
+# Linear models for the 'rented' dataset
+
+# A meaningful prediction, but low R value
 myRegRentedval <- lm(value ~ num_under18 + hhinc, data = rented)
-#meaningless prediction, but best R value could find
+
+# A meaningless prediction, but best R value that could be found
 myRegRentedbed <- lm(num_bedrooms ~ num_rooms + num_under18 + value + num_under6, data = rented)
 
-##Residuals for the lms
+# Residuals for the linear models
 myRegOwnedincRes <- resid(myRegOwnedinc)
 myRegOwnedbedRes <- resid(myRegOwnedbed)
 myRegRentedvalRes <- resid(myRegRentedval)
 myRegRentedbedRes <- resid(myRegRentedbed)
 
-##Plots came out ugly and had too many datapoints just scattered about for meaningful representation.  
-#Summary calls might be the best way to summarize things
+# Plots came out ugly and had too many datapoints just scattered about for meaningful representation.  
+# Summary calls might be the best way to summarize things
 summary(myRegOwnedinc)
 summary(myRegOwnedbed)
 summary(myRegRentedval)
 summary(myRegREntedbed)
 
-#Having chosen to clean up a number of fields of data relating to the overall size, shape and structure of rented and owned domiciles, we discovered that vast qualtities of highly variable data isn't particularly well suited for linear model predictions.  Perhaps if we had chosen to look at each subborough indidually we would have been able to come up with better predictive models.  As it is, the best R value we could find was barely .76/.77.  This vlaue was seen when predicting the number of bedrooms in both owned and rented units.  Apparently the number of rooms in a home is overall an excellent indicator of how many bedrooms the residence had.  And frankly, while these models use multiple inputs, the number of rooms in the unit was overwhelmingly the predictive factor.  Unfortunately we were unable to come up with any meaningful or useful linear models (e.g. preidtcing the home value or household income based on other qualifying factors), as everything else had an R value well below .01 
+# Having chosen to clean up a number of fields of data relating to the overall size, shape and 
+# structure of rented and owned domiciles, we discovered that vast qualtities of highly variable 
+# data isn't particularly well suited for linear model predictions.  Perhaps if we had chosen to look at 
+# each subborough indidually we would have been able to come up with better predictive models. 
+# As it is, the best R value we could find was barely .76/.77.  This vlaue was seen when predicting the number 
+# of bedrooms in both owned and rented units.  Apparently the number of rooms in a home is overall an excellent 
+# indicator of how many bedrooms the residence had.  And frankly, while these models use multiple inputs, the 
+# number of rooms in the unit was overwhelmingly the predictive factor.  Unfortunately we were unable to come up 
+# with any meaningful or useful linear models (e.g. preidtcing the home value or household income based on other 
+# qualifying factors), as everything else had an R value well below .01 
