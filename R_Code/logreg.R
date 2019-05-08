df278b43277a6e03213b71f2092ac10576696bdd
Lanceypantsy <34278955+Lanceypantsy@users.noreply.github.com>
2019-05-08 10:56:59 -0600

    Update logreg.R
    updated directory so the file could be read in from a cloned version of repository.

--- R_Code/logreg.R
+++ R_Code/logreg.R
@@ -1,27 +1,23 @@
-library(dplyr)
-library(ggplot2)
+# Read in file
+lrtotal <- read.csv("../lr.csv", headers = T)
 
-
-##Read in file
-lrtotal <- read.csv("C:\\Users\\maybe\\Downloads\\lr.csv", headers = T)
-
-##Separate into owners and renters
+# Separate into owners and renters
 lrown <- lrtotal %>% filter(Value != 9999999)
 lrrent <- lrtotal %>% filter(Monthly.contract.rent != 99999)
 
-##Include only rows where toilet breakdowns and presence of mice were answered
+# Include only rows where toilet breakdowns and presence of mice were answered
 lrrent <- lrrent[lrrent$Toilet.breakdowns %in% c(1,2), ]
 lrrent <- lrrent[lrrent$Presence.of.mice.or.rats %in% c(1,2), ]
 lrown <- lrown[lrown$Toilet.breakdowns %in% c(1,2), ]
 lrown <- lrown[lrown$Presence.of.mice.or.rats %in% c(1,2), ]
 
-##Change no values from 2 to 0
+# Change no values from 2 to 0
 lrrent$Toilet.breakdowns[lrrent$Toilet.breakdowns == 2] <- 0
 lrrent$Presence.of.mice.or.rats[lrrent$Presence.of.mice.or.rats == 2] <- 0
 lrown$Toilet.breakdowns[lrown$Toilet.breakdowns == 2] <- 0
 lrown$Presence.of.mice.or.rats[lrown$Presence.of.mice.or.rats == 2] <- 0
 
-##Plots
+# Plots
 ggplot(data = lrrent, mapping = aes(y = Toilet.breakdowns, x = Monthly.contract.rent)) +
   geom_point() +
   labs(title = "Toilet Breakdowns vs Monthly Rent") +
