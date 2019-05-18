fe44a253d1df319d1652674bcb2fc8c1234dc6de
rrosebrook <31425480+rrosebrook@users.noreply.github.com>
2019-05-17 18:34:49 -0600

    Update MasterProject.R

--- R_Code/MasterProject.R
+++ R_Code/MasterProject.R
@@ -647,6 +647,49 @@ myRegOwnedinc <- lm(hhinc ~ value + num_rooms + num_bedrooms +
 myRegOwnedbed <- lm(num_bedrooms ~  num_rooms + num_stories + 
                     num_under18, data = owned)
 
+## K means
+                                            
+                                            library(dplyr)
+install.packages("factoextra")
+library(factoextra)
+
+lrtotal <- read.csv("C:\\Users\\maybe\\Downloads\\lr.csv")
+#head(lrtotal)
+
+##Separate into owners and renters
+lrown <- lrtotal %>% filter(Value != 9999999, Total.Household.Income.Recode!=9999999)
+#head(lrown)
+
+#par(mfrow = c(1, 5))
+
+BronxOwn <- lrown %>% filter(Borough == 1)
+BronxOwn2 <- BronxOwn[ , c(3, 7)]
+kcBronxOwn <- kmeans(BronxOwn2, centers = 3)
+fviz_cluster(kcBronxOwn, data = BronxOwn2, main = "Bronx")
+
+Brooklynown <- lrown %>% filter(Borough == 2)
+Brooklynown2 <- Brooklynown[ , c(1,3,7)]
+kcBrooklynOwn <- kmeans(Brooklynown2, centers = 3)
+fviz_cluster(kcBrooklynOwn, data = Brooklynown2[, 2:3], main = "Brooklyn")
+
+ManhatOwn <- lrown %>% filter(Borough == 3)
+ManhatOwn2 <- ManhatOwn[ , c(3, 7)]
+kcManhatOwn <- kmeans(ManhatOwn2, centers = 3)
+fviz_cluster(kcManhatOwn, data = ManhatOwn2, main = "Manhattan")
+
+QueensOwn <- lrown %>% filter(Borough == 4)
+QueensOwn2 <- QueensOwn[ , c(3, 7)]
+kcQueensOwn2 <- kmeans(QueensOwn2, centers = 3)
+fviz_cluster(kcQueensOwn2, data = QueensOwn2, main = "Queens")
+
+SIOwn <- lrown %>% filter(Borough == 5)
+SIOwn2 <- SIOwn[ , c(3, 7)]
+kcSIOwn <- kmeans(SIOwn2, centers = 3)
+fviz_cluster(kcSIOwn, data = SIOwn2, main = "Staten Island")
+?fviz_cluster
+
+                                            
+                                            
 # Linear models for the 'rented' dataset
 
 # A meaningful prediction, but low R value
@@ -800,7 +843,7 @@ plot(arr_tree3,cex = .5)
 # Broken windows for owned homes
 # 2 branches (not insightful, we knew there would be a group with broken, 
 # and a group without broken
-arr_dist4 <- dist(data.frame1$ï..broken_windows, method = "euclidean")
+arr_dist4 <- dist(data.frame1$Ã¯..broken_windows, method = "euclidean")
 arr_clust4 <- hclust(arr_dist4)
 arr_tree4 <-as.phylo(arr_clust4)
 plot(arr_tree4,cex = .5)
